d29f5d3ab0ee0af0a17461edacf6a1166da64516
tirgani12 <ahmed.abhr6@gmail.com>
2023-02-06 20:29:50 -0500

    class20_1

---
+++ class20_1/University.java
@@ -0,0 +1,13 @@
+package class20_1;
+
+import class20.JavaTeacher;
+
+public class University {
+    public static void main(String[] args) {
+        JavaTeacher jt=new JavaTeacher();
+        jt.homework();//public
+        //jt.grade();//protected not visible in different package
+        //jt.scholarship();//default not visible in different package
+        //jt.extraPoint();
+    }
+}
