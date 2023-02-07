d18b17060b9decd2b22fbc9d99f16bef3a876533
tirgani12 <ahmed.abhr6@gmail.com>
2023-02-06 20:38:52 -0500

    taskclass19

---
+++ taskClass19/classA.java
@@ -0,0 +1,36 @@
+package taskClass19;
+
+public class classA {
+    /*
+     HW: Write program for multilevel inheritance where class C inherits from class B
+      and Class B inherits from Class A.
+
+Write program to inherit class A that has method printF which is static
+and call or reuse that method into class B
+
+     */
+    static void printF(){
+        System.out.println('f');
+
+    }
+
+
+}
+class classB extends classA{
+
+
+
+
+    }
+
+
+class classC extends classB{
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
