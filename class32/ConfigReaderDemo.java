4d825428579eaa015c6c2574e5975d5c51c850c6
tirgani12 <ahmed.abhr6@gmail.com>
2023-03-12 16:11:50 -0400

    class32

---
+++ class32/ConfigReaderDemo.java
@@ -0,0 +1,18 @@
+package class32;
+
+
+
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReaderDemo {
+
+
+    public static void main(String[] args) throws IOException {
+
+
+       // Properties properties= ConfigReader.read();
+       // System.out.println(properties.getProperty("user"));
+    }
+}
---
+++ class32/ExcelFileDemo9.java
@@ -0,0 +1,23 @@
+package class32;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class ExcelFileDemo9 {
+    public static void main(String[] args) throws IOException {
+
+        // Break till 01:52
+        //List<Map<String,String>> excelData= ExcelReader.read();
+
+       // for (Map<String,String> row:excelData){
+          //  for (Map.Entry<String,String> entry:row.entrySet()){
+            //    System.out.println(entry.getKey()+" "+entry.getValue());
+            }
+           // System.out.println("**********");
+        }
+
+
+  //  }
+//}
